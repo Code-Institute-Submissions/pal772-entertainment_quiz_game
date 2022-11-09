40d6beb410c0c55838e1fec621bddea4ed7a9620
pal772 <pal722998@gmail.com>
2022-11-09 20:02:18 +0000

    Added function to choosing question one

--- run.py
+++ run.py
@@ -2,3 +2,13 @@ import json
 import time
 
 CATEGORIES_LIST = ['movies', 'music', 'sport', 'television'] 
+
+def ask_one_question(question):
+    print("\n" + question)
+    choice = input("Enter Your Choice [a/b/c/d]: ")
+    while(True):
+        if choice.lower() in ['a', 'b', 'c', 'd']:
+            return choice
+        else:
+            print("Invalid choice. Choose again")
+            choice = input("Enter Choice [a/b/c/d]: ")
