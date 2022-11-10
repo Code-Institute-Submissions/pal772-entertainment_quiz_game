8b412b6000baf2ca14b843a92912a6f3e9c28f3f
pal772 <pal722998@gmail.com>
2022-11-10 16:43:14 +0000

    Added functions for instructions,  entertainment categories and user begin prompt

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import json
 import time
 
-CATEGORIES_LIST = ['movies', 'music', 'sport', 'television'] 
+CATEGORIES_LIST = ['movies', 'music', 'sport', 'television']
 
 def ask_one_question(question):
     print("\n" + question)
@@ -12,3 +12,69 @@ def ask_one_question(question):
         else:
             print("Invalid choice. Choose again")
             choice = input("Enter Choice [a/b/c/d]: ")
+
+def score_one_result(key, meta):
+    actual = meta["answer"]
+    if meta["user_response"].lower() == actual.lower():
+        print("Q.{0} Correct!\n".format(key))
+        return 1
+    else:
+        print("Q.{0} Incorrect!".format(key))
+        print("Correct Answer is ({0})".format(actual))
+        return -1
+
+def test(questions):
+    score = 0
+    print("Instructions:\n1. Please enter only the choice letter to the correct answer.\n2. Each question carries 1 point\n3. Wrong answer, takes 1 point off.\nQuiz will start momentarily. GOOD LUCK!\n")
+    time.sleep(10)
+    for key, meta in questions.items():
+        questions[key]["user_response"] = ask_one_question(meta["question"])
+
+    print("\n-------------- RESULT ----------------\n")
+    for key, meta in questions.items():
+        score += score_one_result(key, meta)
+    print("Your Score:", score, "/", 1 * len(questions))
+
+def load_question(filename):
+    """
+    loads the questions from the JSON file into a Python dictionary and returns it
+    """
+    questions = None
+    with open(filename, "r") as read_file:
+        questions = json.load(read_file)
+    return questions
+
+
+def play_quiz():
+    flag = False
+    try:
+        choice = int(input("Welcome to Today's Entertainment Quiz!\nChoose your Entertainment of interest:\n(1). Movies\n(2). Music\n(3). Sport\n(4). Television\nEnter Your Choice [1/2/3/4]: "))
+        if choice > len(CATEGORIES_LIST) or choice < 1:
+            print("Invalid Choice. Enter Again")
+            flag = True
+    except ValueError as e:
+        print("Invalid Choice. Enter Again")
+        flag = True
+
+    if not flag:
+        questions = load_question('categories/'+CATEGORIES_LIST[choice-1]+'.json')
+        test(questions)
+    else:
+        play_quiz()
+
+def user_begin_prompt():
+    print("Want to test your Entertainment knowledge?\nA. Yes\nB. No")
+    play = input()
+    if play.lower() == 'a' or play.lower() ==  'y':
+        play_quiz()
+    elif play.lower() == 'b':
+        print("Hope you come back soon!")
+    else:
+        print("I didn't quite understand that.\nPress A to play, or B to quit.")
+        user_begin_prompt()
+
+def execute():
+    user_begin_prompt()
+
+if __name__ == '__main__':
+    execute()
