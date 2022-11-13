3fceca1760d8b810edea132e4e49e21cb3957e8a
pal772 <pal722998@gmail.com>
2022-11-13 14:26:38 +0000

    Added comments to run.py

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
+# import requirements for this program
 import json
 import time
+
 
 CATEGORIES_LIST = ['movies', 'music', 'sport', 'television']
 
 def ask_one_question(question):
+    """
+    Function to answering the queations, in lower OR upper case.
+    """
     print("\n" + question)
     choice = input("Enter Your Choice [a/b/c/d]: ")
     while(True):
@@ -11,9 +16,14 @@ def ask_one_question(question):
             return choice
         else:
             print("Invalid choice. Choose again")
-            choice = input("Enter Choice [a/b/c/d]: ")
+            choice = input("Enter Choice [a/b/c/d]: ")      
 
 def score_one_result(key, meta):
+    """
+    This will display the users response 
+    in the correct or wrong answer.
+    """
+
     actual = meta["answer"]
     if meta["user_response"].lower() == actual.lower():
         print("Q.{0} Correct!\n".format(key))
@@ -24,8 +34,11 @@ def score_one_result(key, meta):
         return -1
 
 def test(questions):
+    """
+    This will display a how the quiz works and score for each correct and wrong answer.
+    """
     score = 0
-    print("Instructions:\n1. Please enter only the choice letter to the correct answer.\n2. Each question carries 1 point\n3. Wrong answer, takes 1 point off.\nQuiz will start momentarily. GOOD LUCK!\n")
+    print("Instructions:\n1. Please enter only the choice letter to the correct answer.\n2. Each question carries 1 point\n3. Wrong answer, takes 1 point off.\nQuiz will start momentarily......... GOOD LUCK!\n")
     time.sleep(10)
     for key, meta in questions.items():
         questions[key]["user_response"] = ask_one_question(meta["question"])
@@ -46,6 +59,10 @@ def load_question(filename):
 
 
 def play_quiz():
+
+    """
+    List of categories to choose from.
+    """
     flag = False
     try:
         choice = int(input("Welcome to Today's Entertainment Quiz!\nChoose your Entertainment of interest:\n(1). Movies\n(2). Music\n(3). Sport\n(4). Television\nEnter Your Choice [1/2/3/4]: "))
@@ -63,7 +80,11 @@ def play_quiz():
         play_quiz()
 
 def user_begin_prompt():
-    print("Want to test your Entertainment knowledge?\nA. Yes\nB. No")
+
+    """
+    Introduction function on testing your Entertainment Knowledge
+    """
+    print("Hi.... Want to test your Entertainment knowledge?\nA. Yes\nB. No")
     play = input()
     if play.lower() == 'a' or play.lower() ==  'y':
         play_quiz()
